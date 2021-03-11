2f07b398c4af1efdbe33fba64e792eec013896b1
SheilaTran-UCF <ngochue2076@gmai.com>
2021-03-11 22:59:18 +0700

    add swiper

--- InstagramRN/src/components/NewFeed/Pictures/index.js
+++ InstagramRN/src/components/NewFeed/Pictures/index.js
@@ -1,16 +1,55 @@
 import React from 'react';
-import {View, Image} from 'react-native';
+import {View, Image, Text} from 'react-native';
 import {styles} from './styles';
+import Swiper from 'react-native-swiper';
+
 export default function index() {
   return (
-    <View>
-      <Image
-        style={styles.image}
-        source={{
-          uri:
-            'https://pyxis.nymag.com/v1/imgs/630/6e0/eb215ad90cd826b9e57ff505f54c5c7228-07-avatar.rsquare.w700.jpg',
-        }}
-      />
+    <View style={styles.container}>
+      <Swiper
+        style={styles.wrapper}
+        height={250}
+        dot={<View style={styles.dots} />}
+        activeDot={<View style={styles.Dot} />}
+        paginationStyle={styles.paginationStyle}
+        loop>
+        <View style={styles.slide}>
+          <Image
+            style={styles.image}
+            source={{
+              uri:
+                'https://pyxis.nymag.com/v1/imgs/630/6e0/eb215ad90cd826b9e57ff505f54c5c7228-07-avatar.rsquare.w700.jpg',
+            }}
+          />
+        </View>
+        <View style={styles.slide}>
+          <Image
+            style={styles.image}
+            source={{
+              uri:
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvH0nByLtKT3_KyaOg2VVdRe6wmhBlLTjmhg&usqp=CAU',
+            }}
+          />
+        </View>
+        <View style={styles.slide}>
+          <Image
+            style={styles.image}
+            source={{
+              uri:
+                'https://cdn.mos.cms.futurecdn.net/znJTFDTemj3st5vMC9KxnB.jpg',
+            }}
+          />
+        </View>
+        <View style={styles.slide}>
+          <Image
+            style={styles.image}
+            source={{
+              uri:
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT6M2dnvH86kpi0_rIh8287M5l2zf2B1CGBg&usqp=CAU',
+            }}
+          />
+        </View>
+      </Swiper>
     </View>
   );
 }
--- InstagramRN/src/components/NewFeed/Pictures/styles.js
+++ InstagramRN/src/components/NewFeed/Pictures/styles.js
@@ -8,4 +8,68 @@ export const styles = StyleSheet.create({
     height: 250,
     borderRadius: 15,
   },
+  container: {
+    flex: 1,
+    minHeight: 250,
+  },
+
+  wrapper: {},
+
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
+
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  dots: {
+    backgroundColor: 'rgba(0,0,0,.2)',
+    width: 5,
+    height: 5,
+    borderRadius: 4,
+    marginLeft: 3,
+    marginRight: 3,
+    marginTop: 3,
+    marginBottom: 3,
+  },
+  Dot: {
+    backgroundColor: '#000',
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginLeft: 3,
+    marginRight: 3,
+    marginTop: 3,
+    marginBottom: 3,
+  },
+  paginationStyle: {
+    bottom: -25,
+    left: 0,
+    right: 0,
+  },
 });
