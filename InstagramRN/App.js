f7a2e4192964678464675f0ef124174cee0b7050
SheilaTran-UCF <ngochue2076@gmai.com>
2021-05-29 12:27:55 +0700

    add navigation

--- InstagramRN/App.js
+++ InstagramRN/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import 'react-native-gesture-handler';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 import Home from './src/screens/Home';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={styles.warraper}>
-        <Home />
+        <NavigationContainer>
+          <Home />
+        </NavigationContainer>
       </SafeAreaView>
     </>
   );
